5626d10b2d2df616df75ea080247c9915301aa3f
penanrajput <penanrajput1998@gmail.com>
2024-09-08 15:46:19 +0530

    on click button - background color of layout changes

--- app/src/main/java/com/example/asynctasks/MainActivity.java
+++ app/src/main/java/com/example/asynctasks/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.asynctasks;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnChangeColor;
+    ConstraintLayout clBackground;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clBackground), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnChangeColor = findViewById(R.id.btnChangeColor);
+        clBackground = findViewById(R.id.clBackground);
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                clBackground.setBackgroundColor(Color.RED);
+            }
         });
     }
 }
